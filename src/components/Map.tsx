e7e74b4976fc24f6d35b6f5cf03bfd1da1daecc2
CHNHAO004 <CHNHAO004@MYUCT.AC.ZA>
2024-09-11 04:09:41 +0200

    axios api interaction for map section + markers read from array of data from server
    imported new axios and necessary dependencies. able to get/fetch map data from server, give array of map objects containing coordinate data and location name. implement markers to read from array dynamically instead of static example data place holder. introduced type tMap.

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,8 +1,34 @@
+import axios from "axios";
 import "./mapstyle.css"
 import "leaflet/dist/leaflet.css"
+import React, { useEffect, useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { LatLngExpression } from "leaflet";
+
+export type tMap ={
+  coordinateLat: number;
+  coordinateLong: number;
+  location: String;
+}
 
 export default function Map() {
+  const url = 'http://127.0.0.1:8000/api/' // apiURL
+  // const [map,setmap] = React.useState();
+  const [mapDetail,setmapDetail] = useState<tMap[] | null>();
+
+  // const fetchMapData = () =>{
+  useEffect(() =>{
+    axios.get(url + 'map/')
+    .then((response) => {
+      // console.log(response.data)// test what is in response.data
+      setmapDetail(response.data)
+    })
+    .catch((error) =>{
+      console.error("Something went wrong", error)
+    })
+    console.log(mapDetail)// test what is in map object
+  },[]);
+
   return (
     <MapContainer  
          center={[-33.9289920, 18.4173960]}
@@ -13,11 +39,29 @@ export default function Map() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[-33.9289920, 18.4173960]}>
-        <Popup>
-      	  Example Beach
-        </Popup>
-</Marker>
+
+      
+      {mapDetail && mapDetail.map((item, index) => (
+        <Marker
+          key={index} // Add a unique key to each Marker
+          position={[item.coordinateLat, item.coordinateLong] as LatLngExpression}
+        >
+          <Popup>
+            {item.location} {/* Show location */}
+          </Popup>
+        </Marker>
+      ))}
+
+      {/* {mapDetail ? mapDetail.map((tMap) => {
+        <Marker
+          position={[tMap.coordinateLat, tMap.coordinateLong]}>
+          <Popup>
+      	    tMap.location
+          </Popup>
+        </Marker>
+
+      }) : null} */}
+      
     </MapContainer>
   );
 }
