cc2e607b31c0ff68f7a5b4f0516ca7101ec468d3
CHNHAO004 <CHNHAO004@MYUCT.AC.ZA>
2024-09-18 18:37:55 +0200

    addition of beach status onto marker pins on OSM

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,31 +3,62 @@ import "./mapstyle.css"
 import "leaflet/dist/leaflet.css"
 import React, { useEffect, useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
-import { LatLngExpression } from "leaflet";
+import { LatLngExpression, marker } from "leaflet";
 
 export type tMap ={
   coordinateLat: number;
   coordinateLong: number;
   location: String;
+  beach:number;
+}
+
+export type tBeach ={
+ id:number;
+ status:string;
+ name: string;
+ location: string;
+ description: string;
+ current_temperature: number;
+ current_rain: string;
+ current_wind_speed: number;
+ saturday_temperature: number;
+ saturday_rain: string;
+ saturday_wind_speed: number;
+ sunday_temperature: number;
+ sunday_rain: string;
+ sunday_wind_speed: number;
+ funFacts: string;
 }
 
 export default function Map() {
   const url = 'http://127.0.0.1:8000/api/' // apiURL
-  // const [map,setmap] = React.useState();
   const [mapDetail,setmapDetail] = useState<tMap[] | null>();
-
-  // const fetchMapData = () =>{
+  const [beaches,setbeaches] = useState<tBeach[] | null>();
+  
   useEffect(() =>{
     axios.get(url + 'map/')
     .then((response) => {
-      // console.log(response.data)// test what is in response.data
       setmapDetail(response.data)
     })
     .catch((error) =>{
       console.error("Something went wrong", error)
     })
-    console.log(mapDetail)// test what is in map object
   },[]);
+
+  useEffect(() =>{
+    axios.get(url + 'beaches/')
+    .then((response) => {
+      setbeaches(response.data)
+    })
+    .catch((error) =>{
+      console.error("Something went wrong", error)
+    })
+  },[]);
+
+  // Function to get beach data by location ID
+  const getBeachData = (mapId: number): tBeach | undefined => {
+    return beaches?.find(beach => beach.id === mapId);
+  };
 
   return (
     <MapContainer  
@@ -38,30 +69,54 @@ export default function Map() {
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-
-      
-      {mapDetail && mapDetail.map((item, index) => (
-        <Marker
-          key={index} // Add a unique key to each Marker
-          position={[item.coordinateLat, item.coordinateLong] as LatLngExpression}
-        >
-          <Popup>
-            {item.location} {/* Show location */}
-          </Popup>
-        </Marker>
-      ))}
-
-      {/* {mapDetail ? mapDetail.map((tMap) => {
-        <Marker
-          position={[tMap.coordinateLat, tMap.coordinateLong]}>
-          <Popup>
-      	    tMap.location
-          </Popup>
-        </Marker>
-
-      }) : null} */}
-      
+      />      
+      {mapDetail && mapDetail.map((item, index) => {
+        const beach = getBeachData(item.beach);
+        return (
+          <Marker
+            key={index} // Add a unique key to each Marker
+            position={[item.coordinateLat, item.coordinateLong] as LatLngExpression}
+          >
+            <Popup>
+              <div>
+                <strong>{beach ? beach.name : "N/A"}</strong>
+                <br />
+                <strong>Status:</strong>
+                <br />
+                {beach ? beach.status : "Status Unknown"}
+                <br />
+                <strong>Location:</strong>
+                <br />
+                {beach ? beach.location : "N/A"}
+                <br />
+                <strong>Weather Conditions</strong>
+                <br />
+                <strong>[Temperatur,Rainfall,Wind Speed]</strong>
+                <br />
+                <strong>Today:</strong>
+                <br />
+                {beach ? `${beach.current_temperature} °C, ${beach.current_rain}, ${beach.current_wind_speed} km/h` : "N/A"}
+                <br />
+                <strong>Saturday:</strong> 
+                <br />
+                {beach ? `${beach.saturday_temperature} °C, ${beach.saturday_rain}, ${beach.saturday_wind_speed} km/h` : "N/A"}
+                <br />
+                <strong>Sunday: </strong>
+                <br />
+                {beach ? `${beach.sunday_temperature} °C, ${beach.sunday_rain}, ${beach.sunday_wind_speed} km/h` : "N/A"}
+                <br />
+                <strong>Description:</strong> 
+                <br />
+                {beach ? beach.description : "N/A"}
+                <br />
+                <strong>Fun Facts:</strong> 
+                <br />
+                {beach ? beach.funFacts : "N/A"}
+              </div>
+            </Popup>
+          </Marker>
+        );
+      })}
     </MapContainer>
   );
 }
